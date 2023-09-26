287133602949831cda7233406edab4ae1a9e047f
desaidhanashri68 <133623097+desaidhanashri68@users.noreply.github.com>
2023-09-26 06:58:07 +0530

    Add files via upload

---
+++ product/src/main/java/com/oit/ProductService.java
@@ -0,0 +1,38 @@
+package com.oit;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        updatedProduct.setId(id); // Ensure the ID is set
+        return productRepository.save(updatedProduct);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
